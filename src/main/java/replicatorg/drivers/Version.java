027c71480bb67ad9eb83abac5a7981e5e98eaaa2
Liam Marshall <archimedespi3141@gmail.com>
2015-08-20 17:26:09 -0500

    Add @Override where needed in Version, reindent with SPACES NOT TABS

--- src/main/java/replicatorg/drivers/Version.java
+++ src/main/java/replicatorg/drivers/Version.java
@@ -3,42 +3,44 @@ package replicatorg.drivers;
 import java.util.Objects;
 
 public class Version implements Comparable<Version> {
-	private int minor;
-	private int major;
+  private int minor;
+  private int major;
 
-	public Version( int major, int minor ) {
-		this.major = major;
-		this.minor = minor;
-	}
+  public Version( int major, int minor ) {
+    this.major = major;
+    this.minor = minor;
+  }
 
   @Override
-	public boolean equals(Object o) {
-		if (o instanceof Version) {
-			Version v = (Version)o;
-			return major == v.major && minor == v.minor;
-		}
-		return false;
-	}
+  public boolean equals(Object o) {
+    if (o instanceof Version) {
+      Version v = (Version)o;
+      return major == v.major && minor == v.minor;
+    }
+    return false;
+  }
 
   @Override
   public int hashCode() {
     return Objects.hash(this.major, this.minor);
   }
 
-	public boolean atLeast(Version v) {
-		return compareTo(v) >= 0;
-	}
-	
-	public int compareTo(Version v) {
-		if (major > v.major || ((major == v.major) && (minor > v.minor))) return 1;
-		if (equals(v)) return 0;
-		return -1;
-	}
-	
-	public String toString() {
-		return Integer.toString(major) + "." + Integer.toString(minor);
-	}
+  public boolean atLeast(Version v) {
+    return compareTo(v) >= 0;
+  }
 
-	public int getMajor() { return major; }
-	public int getMinor() { return minor; }
+  @Override
+  public int compareTo(Version v) {
+    if (major > v.major || ((major == v.major) && (minor > v.minor))) return 1;
+    if (equals(v)) return 0;
+    return -1;
+  }
+
+  @Override
+  public String toString() {
+    return Integer.toString(major) + "." + Integer.toString(minor);
+  }
+  
+  public int getMajor() { return major; }
+  public int getMinor() { return minor; }
 }
