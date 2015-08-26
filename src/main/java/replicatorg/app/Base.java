ec5c7076a96007640fe9967bdf32cdadb2e3dc01
Liam Marshall <archimedespi3141@gmail.com>
2015-08-26 15:41:44 -0500

    yay much version bump

--- src/main/java/replicatorg/app/Base.java
+++ src/main/java/replicatorg/app/Base.java
@@ -76,7 +76,7 @@ public class Base {
   /**
    * The version number of this edition of replicatorG.
    */
-  public static final int VERSION = 40;
+  public static final int VERSION = 50;
 
   /**
    * The textual representation of this version (4 digits, zero padded).
